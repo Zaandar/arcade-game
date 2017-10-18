45f9af5220a0762659b103f2f7ad57364671c4a7
Sandra Velez <sandyvelez@gmail.com>
2017-10-17 21:25:20 -0400

    Put enemies on the board

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
+const numRows = 6;
+const numCols = 5;
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(LocX, LocY) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = LocX;
+    this.y = LocY;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // console.log("time delta: " + dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +31,56 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player{
+    constructor(){
+        this.sprite = 'images/char-horn-girl.png';
+    }
 
+    update(){}
+
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(){}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let halfRowHeight = 21.5;
+let bottomOfRow = 83;
+let columnWidth = 101;
 
+let row1x = 0 * columnWidth;
+let row1y = (0 * bottomOfRow) - halfRowHeight;
 
+let row2x = 1 * columnWidth;
+let row2y = (1 * bottomOfRow) - halfRowHeight;
+
+let row3x = 2 * columnWidth;
+let row3y = (2 * bottomOfRow) - halfRowHeight;
+
+let row4x = 3 * columnWidth;
+let row4y = (3 * bottomOfRow) - halfRowHeight;
+
+let row5x = 4 * columnWidth;
+let row5y = (4 * bottomOfRow) - halfRowHeight;
+
+let player = new Player();
+let allEnemies = [];
+
+for (let row = 1; row <numRows-2; row++){
+    for (let col = 0; col < numCols; col++) {
+
+        let x = col * columnWidth;
+        let y = (row * bottomOfRow) - halfRowHeight;
+
+        allEnemies.push(new Enemy(x, y));
+    }
+}
+
+console.log(allEnemies);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
