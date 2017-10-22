8c89d254fae96928ea0ea914de1cbef2761e4df5
Sandra Velez <sandyvelez@gmail.com>
2017-10-21 20:13:56 -0400

    randomize enemy creation

--- js/app.js
+++ js/app.js
@@ -7,70 +7,54 @@ const boardWidth = numCols * columnWidth;
 const boardHeight = numRows * rowHeight;
 let deltaX = 0;
 let deltaY = 0;
+let allEnemies = [];
+let grid = [];
+
+function generateRandomInRange(min, max) {
+    let rand = Math.random() * (max - min) + min;
+    return rand;
+}
 
 // Enemies our player must avoid
-let Enemy = function(LocX, LocY) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor(locX, locY) {
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = LocX;
-    this.y = LocY;
-};
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+        this.x = locX;
+        this.y = locY;
+        this.speedMultiplier = generateRandomInRange(130, 200);
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x = this.x + (10 * dt);
-};
+    // Update the enemy's position
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // Multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        if (this.speedMultiplier != null) {
+            this.x = this.x + (this.speedMultiplier * dt);
+        }
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-class Player{
-    constructor(){
+// Our player
+class Player {
+    constructor() {
         this.sprite = 'images/char-horn-girl.png';
 
         this.x = 2 * columnWidth;
         this.y = (5 * rowHeight) - rowOffset;
     }
 
-    update(){
-        if (deltaX !== 0) {
-            let newX = this.x + deltaX;
-            if (newX >= 0 && newX <= (boardWidth - columnWidth)) {
-                this.x = newX;
-            }
-        }
-
-        if(deltaY !== 0) {
-            let newY = this.y + deltaY;
-            if (newY >= (0 - rowHeight) && newY <= (boardHeight - rowHeight)) {
-                this.y = newY;
-            }
-        }
-
-        // reset to 0 to stop motion
-        deltaX = 0;
-        deltaY = 0;
-    }
-
-    render(){
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-
-    static handleInput(keyCode){
-        switch (keyCode){
+    static handleInput(keyCode) {
+        switch (keyCode) {
             case "left":
                 deltaX = -columnWidth;
                 deltaY = 0;
@@ -89,29 +73,64 @@ class Player{
                 break;
         }
     }
-}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-let player = new Player();
-let allEnemies = [];
+    update() {
+        if (deltaX !== 0) {
+            let newX = this.x + deltaX;
+            if (newX >= 0 && newX <= (boardWidth - columnWidth)) {
+                this.x = newX;
+            }
+        }
 
-for (let row = 1; row <numRows-2; row++){
-    for (let col = 0; col < numCols; col++) {
+        if (deltaY !== 0) {
+            let newY = this.y + deltaY;
+            if (newY >= (0 - rowHeight) && newY <= (boardHeight - rowHeight)) {
+                this.y = newY;
+            }
+        }
 
-        let x = col * columnWidth;
-        let y = (row * rowHeight) - rowOffset;
+        // reset to 0 to stop motion
+        deltaX = 0;
+        deltaY = 0;
+    }
 
-        allEnemies.push(new Enemy(x, y));
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
-console.log(allEnemies);
+let player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+createEnemyRows();
+
+function createEnemyRows() {
+    for (let row = 1; row < numRows - 2; row++) {
+        let y = (row * rowHeight) - rowOffset;
+        grid.push(y);
+    }
+}
+
+function createEnemy() {
+    // pick a row to put the enemy on
+    let row = Math.floor(generateRandomInRange(0, 3));
+
+    // add enemy
+    allEnemies.push(new Enemy(0, grid[row]));
+
+    if (allEnemies.length > 25) {
+        allEnemies.shift();
+    }
+};
+
+// generate an enemy every few fractions of a second
+let seconds = generateRandomInRange(.5, .75) * 1000;
+setInterval(function () {
+    createEnemy();
+}, seconds);
+
+// This listens for key presses and sends the keys to the
+// Player.handleInput() method.
+document.addEventListener('keyup', function (e) {
     let allowedKeys = {
         37: 'left',
         38: 'up',
